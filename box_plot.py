af18662748b7ac7f893d381b5e648e2f455a4398
kenwalger <kenalger@comcast.net>
2017-11-13 10:23:27 -0800

    Update to box_plot.py

--- box_plot.py
+++ box_plot.py
@@ -1,7 +1,8 @@
 import csv
 import matplotlib.pyplot as plt
+from itertools import groupby
 
-input_file = 'iris.csv'
+input_file = 'iris_data_set/iris.csv'
 plt.figure(figsize=(10.5, 5.5))
 plt.style.use('classic')
 fig = plt.figure()
@@ -9,19 +10,13 @@ fig = plt.figure()
 with open(input_file, 'r') as iris_data:
     irises = list(csv.reader(iris_data))
 
-setosa = []
-versicolor = []
-virginica = []
-
-for petal in range(0, len(irises)-1):
-    if irises[petal][4] == 'Iris-setosa':
-        setosa.append(float(irises[petal][2]))
-    elif irises[petal][4] == 'Iris-versicolor':
-        versicolor.append(float(irises[petal][2]))
-    elif irises[petal][4] == 'Iris-virginica':
-        virginica.append(float(irises[petal][2]))
-
-petal_lengths = [setosa, versicolor, virginica]
+irises.pop()
+petal_lengths=[]
+for species, group in groupby(irises, lambda i: i[4]):
+    # Group is a generator, so you can only go over it once
+    # categorized_irises = list(group)
+    # print(str(categorized_irises))
+    petal_lengths.append([float(petal[2]) for petal in group])
 
 plt.boxplot(petal_lengths)
 
